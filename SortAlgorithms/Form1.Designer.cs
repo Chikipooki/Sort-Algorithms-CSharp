b688b0108c8d4dda74f5709e3dc8cd1c0f80b7e5
Chikipooki <AltarNatsev@yandex.ru>
2022-12-07 12:18:58 +0700

    Vertical progress bar added at panel also logic

--- SortAlgorithms/Form1.Designer.cs
+++ SortAlgorithms/Form1.Designer.cs
@@ -28,81 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.AddLabel = new System.Windows.Forms.Label();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.AddTextBox = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.FillLabel = new System.Windows.Forms.Label();
+            this.FillButton = new System.Windows.Forms.Button();
+            this.FillTextBox = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // panel1
             // 
-            this.button1.Location = new System.Drawing.Point(128, 15);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.panel1.Controls.Add(this.AddLabel);
+            this.panel1.Controls.Add(this.AddButton);
+            this.panel1.Controls.Add(this.AddTextBox);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(493, 96);
+            this.panel1.TabIndex = 0;
             // 
-            // textBox1
+            // AddLabel
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 26);
-            this.textBox1.TabIndex = 2;
+            this.AddLabel.AutoSize = true;
+            this.AddLabel.Location = new System.Drawing.Point(3, 0);
+            this.AddLabel.Name = "AddLabel";
+            this.AddLabel.Size = new System.Drawing.Size(133, 20);
+            this.AddLabel.TabIndex = 2;
+            this.AddLabel.Text = "Добавить число";
             // 
-            // button2
+            // AddButton
             // 
-            this.button2.Location = new System.Drawing.Point(12, 74);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(191, 41);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.AddButton.Location = new System.Drawing.Point(367, 23);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(106, 36);
+            this.AddButton.TabIndex = 1;
+            this.AddButton.Text = "Добавить";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
             // 
-            // label1
+            // AddTextBox
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 51);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 20);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Значениие";
+            this.AddTextBox.Location = new System.Drawing.Point(3, 23);
+            this.AddTextBox.Name = "AddTextBox";
+            this.AddTextBox.Size = new System.Drawing.Size(358, 26);
+            this.AddTextBox.TabIndex = 0;
             // 
-            // label2
+            // panel2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 127);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 20);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Результат ";
+            this.panel2.Controls.Add(this.FillLabel);
+            this.panel2.Controls.Add(this.FillButton);
+            this.panel2.Controls.Add(this.FillTextBox);
+            this.panel2.Location = new System.Drawing.Point(12, 114);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(493, 94);
+            this.panel2.TabIndex = 3;
+            // 
+            // FillLabel
+            // 
+            this.FillLabel.AutoSize = true;
+            this.FillLabel.Location = new System.Drawing.Point(3, 0);
+            this.FillLabel.Name = "FillLabel";
+            this.FillLabel.Size = new System.Drawing.Size(316, 20);
+            this.FillLabel.TabIndex = 2;
+            this.FillLabel.Text = "Заполнить массив случайными числами\r\n";
+            // 
+            // FillButton
+            // 
+            this.FillButton.Location = new System.Drawing.Point(367, 23);
+            this.FillButton.Name = "FillButton";
+            this.FillButton.Size = new System.Drawing.Size(106, 36);
+            this.FillButton.TabIndex = 1;
+            this.FillButton.Text = "Заполнить";
+            this.FillButton.UseVisualStyleBackColor = true;
+            this.FillButton.Click += new System.EventHandler(this.FillButton_Click);
+            // 
+            // FillTextBox
+            // 
+            this.FillTextBox.Location = new System.Drawing.Point(3, 23);
+            this.FillTextBox.Name = "FillTextBox";
+            this.FillTextBox.Size = new System.Drawing.Size(358, 26);
+            this.FillTextBox.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Location = new System.Drawing.Point(511, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(614, 196);
+            this.panel3.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1137, 490);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label AddLabel;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.TextBox AddTextBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label FillLabel;
+        private System.Windows.Forms.Button FillButton;
+        private System.Windows.Forms.TextBox FillTextBox;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
