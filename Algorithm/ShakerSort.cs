d607727a4ce2fbede384dd3708ec06e565ec0cab
Chikipooki <AltarNatsev@yandex.ru>
2022-12-11 12:00:39 +0700

    Changed compare method

--- Algorithm/ShakerSort.cs
+++ Algorithm/ShakerSort.cs
@@ -14,10 +14,9 @@ namespace Algorithm
                 var preOutCounter = SwopCount;
                 for (int i = left; i < right; i++)
                 {
-                    if (Items[i].CompareTo(Items[i + 1]) == 1)
+                    if (Compare(Items[i], Items[i + 1]) == 1)
                     {
                         Swop(i, i + 1);
-                        ComparisonCount++;
                     }
                 }
                 right--;
@@ -29,10 +28,9 @@ namespace Algorithm
 
                 for (int i = right; i > left; i--)
                 {
-                    if (Items[i].CompareTo(Items[i - 1]) == -1)
+                    if (Compare(Items[i], Items[i - 1]) == -1)
                     {
                         Swop(i, i - 1);
-                        ComparisonCount++;
                     }
                 }
                 left++;
