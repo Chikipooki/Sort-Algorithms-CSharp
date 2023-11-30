3cd85814eb0c052c7658f01f57f0666cbd6f1f83
Alexey Taradoyna <AltarNatsev@yandex.ru>
2023-11-30 17:05:17 +0700

    Add a test of the shaker sort algorithm
    We create random numbers, sort and compare the result with standard C# sorting

---
+++ AlgorithmTests/ShakerSortTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Tests
+{
+    [TestClass()]
+    public class ShakerSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+
+            //arrenge
+            var shaker = new ShakerSort<int>();
+            var rnd = new Random();
+            var items = new List<int>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                items.Add(rnd.Next(0, 100));
+            }
+
+            shaker.Items.AddRange(items);
+            items.Sort();
+
+            //act
+            shaker.Sort();
+
+            //assert
+            for (int i = 0; i < items.Count; i++)
+            {
+                Assert.AreEqual(items[i], shaker.Items[i]);
+            }
+        }
+    }
+}
