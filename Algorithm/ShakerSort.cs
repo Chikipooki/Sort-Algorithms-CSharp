305231376c1b4a88bce4399e52140aedbf8e618f
Alexey Taradoyna <AltarNatsev@yandex.ru>
2023-11-30 17:03:49 +0700

    feat: Add ShakerSort class
    Cocktail-Shaker sort function implemented

---
+++ Algorithm/ShakerSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Algorithm
+{
+    public class ShakerSort<T> : AlgorithmBase<T> where T : IComparable 
+    {
+        public override void Sort()
+        {
+            var left = 0;
+            var right = Items.Count - 1;
+
+            while (left < right)
+            {
+                for (var i = left; i < right; i++)
+                {
+                    if (Items[i].CompareTo(Items[i + 1]) == 1)
+                    {
+                        Swop(i, i + 1);
+                    }
+                }
+                right--;
+
+                for(int i = right; i > left; i--)
+                {
+                    if (Items[i].CompareTo(Items[i-1]) == -1)
+                    {
+                        Swop(i, i - 1);
+                    }
+                }
+                left++;
+            }
+        }
+    }
+}
