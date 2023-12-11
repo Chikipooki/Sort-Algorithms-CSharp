9cc10b0686630e3af25cab8013b143c1d2db5432
Alexey Taradoyna <AltarNatsev@yandex.ru>
2023-12-11 20:30:04 +0700

    feat: add constructor
    add a constructor to the AlgorithmBase

--- Algorithm/AlgorithmBase.cs
+++ Algorithm/AlgorithmBase.cs
@@ -1,22 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Algorithm
 {
-    public class AlgorithmBase<T> where T: IComparable
+    public class AlgorithmBase<T> where T : IComparable
     {
-        public int SwopCount { get; protected set; } = 0;
         public int ComparsionCount { get; protected set; } = 0;
-
+        public int SwopCount { get; protected set; } = 0;
         public List<T> Items { get; set; } = new List<T>();
+        public AlgorithmBase(IEnumerable<T> items)
+        {
+            Items.AddRange(items);
+        }
+        public AlgorithmBase() { }
+
+        public TimeSpan Sort()
+        {
+            var timer = new Stopwatch();
+            SwopCount = 0;
+
+            timer.Start();
+            MakeSort();
+            timer.Stop();
+
+            return timer.Elapsed;
+        }
+
+        protected virtual void MakeSort()
+        {
+            Items.Sort();
+        }
+
         /// <summary>
-        /// Сравнивает текущий экземпляр с другим объектом того же типа и меняет местами текущий и другой объект.  
+        /// Сравнивает текущий экземпляр с другим объектом того же типа и меняет местами текущий и другой объект.
         /// </summary>
         /// <param name="positionA">Первый объект типа int </param>
         /// <param name="positionB">Второй объект типа int </param>
         protected void Swop(int positionA, int positionB)
         {
-            if(positionA < Items.Count && positionB < Items.Count)
+            if (positionA < Items.Count && positionB < Items.Count)
             {
                 var temp = Items[positionA];
                 Items[positionA] = Items[positionB];
@@ -24,12 +47,6 @@ namespace Algorithm
 
                 SwopCount++;
             }
-        }
-
-        public virtual void Sort()
-        {
-            SwopCount = 0;
-            Items.Sort();
         }
     }
 }
