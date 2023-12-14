9e114d6684f8b0cd1242aeebf7946c473f2ea374
Alexey Taradoyna <AltarNatsev@yandex.ru>
2023-12-14 10:30:25 +0700

    feat: add structure Tree for TreeSort

---
+++ Algorithm/DataStructures/Tree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.DataStructures
+{
+    internal class Tree<T> where T : IComparable
+    {
+        public Node<T> Root { get; private set; }
+        public int Counter { get; private set; }
+        public Tree() { }
+        public Tree(IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                Add(item);
+            }
+        }
+
+        public void Add(T data)
+        {
+            if (Root == null)
+            {
+                Root = new Node<T>(data);
+                Counter = 1;
+                return;
+            }
+
+            Root.Add(data);
+            Counter++;
+        }
+
+        public List<T> Inorder()
+        {
+            if (Root == null)
+            {
+                return new List<T>();
+            }
+
+            return Inorder(Root);
+        }
+
+        private List<T> Inorder(Node<T> node)
+        {
+            var list = new List<T>();
+            if (node != null)
+            {
+                if (node.Left != null)
+                {
+                    list.AddRange(Inorder(node.Left));
+                }
+
+                list.Add(node.Data);
+
+                if (node.Right != null)
+                {
+                    list.AddRange(Inorder(node.Right));
+                }
+            }
+
+            return list;
+        }
+    }
+}
