4b45fddd7fc6955e897a2f3c669b3b868c613eff
Alexey Taradoyna <AltarNatsev@yandex.ru>
2023-12-14 10:30:16 +0700

    feat: add structure Node for TreeSort

---
+++ Algorithm/DataStructures/Node.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.DataStructures
+{
+    internal class Node<T> where T : IComparable
+    {
+        public T Data { get; private set; }
+        public Node<T> Left { get; private set; }
+        public Node<T> Right { get; private set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+
+            if (node.Data.CompareTo(Data) == -1)
+            {
+                if (Left == null)
+                {
+                    Left = node;
+                }
+                else
+                {
+                    Left.Add(data);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = node;
+                }
+                else
+                {
+                    Right.Add(data);
+                }
+            }
+        }
+    }
+}
