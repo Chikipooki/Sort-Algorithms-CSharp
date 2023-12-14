306a683592fab22615137a0293af632fbecb729e
Alexey Taradoyna <AltarNatsev@yandex.ru>
2023-12-14 10:31:09 +0700

    test: Add Tree Sort Test

--- AlgorithmTests/SortTests.cs
+++ AlgorithmTests/SortTests.cs
@@ -113,5 +113,22 @@ namespace Algorithm.Tests
                 Assert.AreEqual(Sorted[i], shell.Items[i]);
             }
         }
+
+        [TestMethod()]
+        public void TreeSortTest()
+        {
+            //arrenge
+            var tree = new TreeSort<int>();
+            tree.Items.AddRange(Items);
+
+            //act
+            tree.Sort();
+
+            //assert
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Assert.AreEqual(Sorted[i], tree.Items[i]);
+            }
+        }
     }
 }
