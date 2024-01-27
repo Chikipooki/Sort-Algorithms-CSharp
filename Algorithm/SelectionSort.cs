6f328875ee1e2f851707f37feb4ab8d9f791e066
Alexey Taradoyna <AltarNatsev@yandex.ru>
2024-01-27 23:04:02 +0700

    refactor: Insertion sort
    Improved the algorithm without changing functionality

--- Algorithm/SelectionSort.cs
+++ Algorithm/SelectionSort.cs
@@ -10,15 +10,13 @@ namespace Algorithm
 
         protected override void MakeSort()
         {
-            var minIndex = 0;
-
             for (int i = 0; i < Items.Count - 1; i++)
             {
-                minIndex = i;
+               var minIndex = i;
 
                 for (int j = i + 1; j < Items.Count; j++)
                 {
-                    if (Items[j].CompareTo(Items[minIndex]) == -1)
+                    if (Compare(Items[j], Items[minIndex]) == -1)
                     {
                         minIndex = j;
                     }
@@ -28,6 +26,5 @@ namespace Algorithm
                     Swop(i, minIndex);
             }
         }
-
     }
 }
