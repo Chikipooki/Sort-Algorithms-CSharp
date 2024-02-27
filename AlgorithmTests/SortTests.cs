df7c8079195aec5d8149ef10684d66e29ac899fc
Alexey Taradoyna <AltarNatsev@yandex.ru>
2024-02-27 17:06:25 +0700

    test: Add merge sort test

--- AlgorithmTests/SortTests.cs
+++ AlgorithmTests/SortTests.cs
@@ -211,5 +211,22 @@ namespace Algorithm.Tests
                 Assert.AreEqual(Sorted[i], msdRedix.Items[i]);
             }
         }
+
+        [TestMethod()]
+        public void MergeSortTest()
+        {
+            //arrenge
+            var merge = new MergeSort<int>();
+            merge.Items.AddRange(Items);
+
+            //act
+            merge.Sort();
+
+            //assert
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Assert.AreEqual(Sorted[i], merge.Items[i]);
+            }
+        }
     }
 }
